79b90a3de3c56d75b3e087e390ebbb26931d8faa
Inácio Fabrício Nunes de Sousa <fabricionunes.si@hotmail.com>
2019-12-06 14:14:29 -0300

    Aula 3

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
+setInterval(draw, 10);
+
+var x = canvas.width/2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2
+
+function draw() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+    x += dx;
+    y += dy;
+}
+
+
+/*
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
 ctx.fillStyle='#FF0000';
@@ -18,3 +37,4 @@ ctx.rect(160, 10, 100, 40);
 ctx.strokeStyke = 'rgba(0, 0, 255, 0.5)';
 ctx.stroke();
 ctx.closePath();
+*/
